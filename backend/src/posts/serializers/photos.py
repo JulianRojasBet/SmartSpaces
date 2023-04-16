f5ca3d44e68d461596a42e45cd41074ebee8aa3e
Santiago Ortiz <santiago_ortiz@epam.com>
2023-04-16 16:21:53 -0500

    ai models

--- backend/src/posts/serializers/photos.py
+++ backend/src/posts/serializers/photos.py
@@ -17,3 +17,13 @@ class SuggestionsSerializer(serializers.Serializer):
 
     photo_url = serializers.URLField(max_length=255, allow_blank=False)
     action = serializers.CharField(max_length=255, allow_blank=False)
+    model_choices = [
+        ('dalle', 'Dall-e'),
+        ('replicate', 'Replicate'),
+        ('stable_diffusion', 'Stable-diffusion')
+    ]
+    AI_model = serializers.ChoiceField(choices=model_choices)
+    x1_coordinate = serializers.IntegerField(required=False)
+    y1_coordinate = serializers.IntegerField(required=False)
+    x2_coordinate = serializers.IntegerField(required=False)
+    y2_coordinate = serializers.IntegerField(required=False)
