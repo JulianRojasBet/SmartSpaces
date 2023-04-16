64103c24fbb07050c8051cbba4683d3082023cca
Julian Rojas <julian.rojas@partners.goodrx.com>
2023-04-16 14:55:20 -0500

    finish all components

--- front/smart-spaces/src/app/space/[id]/components/AdviceForm.tsx
+++ front/smart-spaces/src/app/space/[id]/components/AdviceForm.tsx
@@ -1,24 +1,44 @@
 "use client";
 
+import { useState } from "react";
+import Image from "next/image";
 import { Button } from "@/components/Button";
 import { Modal } from "@/components/Modal";
 import { Select } from "@/components/Select";
+import { PlaceImage } from "@/app/components/types";
 import { ImageList } from "./ImageList";
 
-export function AdviceForm() {
+type AdviceFormProps = {
+  improved: PlaceImage;
+};
+
+export function AdviceForm({ improved }: AdviceFormProps) {
   const modalId = "preview-modal";
+  const improveModalId = "improve-modal-id";
+  const emptyImage = { id: "", url: "" };
   const images = [
-    { id:"image1", url: 'https://via.placeholder.com/200x200'},
-    { id:"image2", url: 'https://via.placeholder.com/200x200'},
-    { id:"image3", url: 'https://via.placeholder.com/200x200'},
-    { id:"image4", url: 'https://via.placeholder.com/200x200'},
-  ]
+    { id: "image1", url: "https://via.placeholder.com/200x200" },
+    { id: "image2", url: "https://via.placeholder.com/200x200" },
+    { id: "image3", url: "https://via.placeholder.com/200x200" },
+    { id: "image4", url: "https://via.placeholder.com/200x200" },
+  ];
   const options = [{ value: "living-room", label: "Living room" }];
+
+  const [selected, setSelected] = useState<PlaceImage>(emptyImage);
+
+  const handleSelect = (id: string) => {
+    const image = images.find((image) => image.id === id) || emptyImage;
+    setSelected(image);
+  };
+
+  const handleClose = () => {
+    setSelected(emptyImage)
+  }
 
   return (
     <div className="flex flex-col max-w-2xl mx-auto">
       <h2 className="text-4xl font-bold text-center mb-10">
-        Improve you place by room
+        Improve you space by room
       </h2>
       <div className="flex flex-row justify-center mb-8">
         <Select
@@ -90,9 +110,47 @@ export function AdviceForm() {
         <h3 className="font-bold text-lg mb-4">
           Select the image where you want to apply the advice
         </h3>
-        <ImageList images={images} />
+        <ImageList
+          selected={selected}
+          images={images}
+          onSelect={handleSelect}
+        />
         <div className="modal-action">
-          <a href="#" className="btn btn-secondary">
+          <a
+            href={`#${improveModalId}`}
+            className={`btn btn-primary ${!selected.id ? "btn-disabled" : ""}`}
+          >
+            Select
+          </a>
+          <a href="#" className="btn btn-secondary" onClick={handleClose}>
+            Close
+          </a>
+        </div>
+      </Modal>
+      <Modal id={improveModalId}>
+        <h3 className="font-bold text-lg mb-4">
+          This is how your space could look like
+        </h3>
+        <div className="flex flex-row justify-between">
+          <div className="min-w-[400px] min-h-[400px] relative m-2">
+            <Image
+              src={selected.url}
+              alt={selected.id}
+              fill
+              className="rounded-lg"
+            />
+          </div>
+          <div className="min-w-[400px] min-h-[400px] relative m-2">
+            <Image
+              src={improved.url}
+              alt={improved.id}
+              fill
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+        <div className="modal-action">
+          <a href="#" className="btn btn-secondary" onClick={handleClose}>
             Close
           </a>
         </div>
--- front/smart-spaces/src/components/Modal.tsx
+++ front/smart-spaces/src/components/Modal.tsx
@@ -9,7 +9,7 @@ export function Modal({ id, children }: ModalProps) {
   return (
     <>
       <div className="modal" id={id}>
-        <div className="modal-box  w-11/12 max-w-5xl">
+        <div className="modal-box max-w-5xl">
           {children}
         </div>
       </div>
